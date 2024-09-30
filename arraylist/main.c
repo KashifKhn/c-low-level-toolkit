cc5b64bd7e7958587f59fc26713c774ca7e95796
KashifKhn <kashifkhnx04@gmail.com>
2024-09-30 10:56:42 +0500

    test get elements

--- arraylist/main.c
+++ arraylist/main.c
@@ -12,13 +12,18 @@ int main(int argc, char *argv[]) {
     value = i * 10;
     add(arr, &value);
     assert(size(arr) == i + 1);
+    assert(*(int *)get(arr, i) == value);
   }
+
   assert(arr->capacity == 10);
   for (int i = 10; i < 20; i++) {
     value = i * 10;
     add(arr, &value);
     assert(size(arr) == i + 1);
+    assert(*(int *)get(arr, i) == value);
   }
+
+  assert(*(int *)get(arr, 19) == 190);
 
   assert(arr->capacity == 20);
   assert(size(arr) == 20);
